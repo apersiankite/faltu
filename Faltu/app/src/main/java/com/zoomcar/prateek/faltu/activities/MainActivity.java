5770e08371f4da3c2693038674b0556c16e8a18f
Prateek Saini <prateek.saini@zoomcar.com>
2015-09-10 23:48:12 +0530

    Added toolbar in main activity

--- Faltu/app/src/main/java/com/zoomcar/prateek/faltu/activities/MainActivity.java
+++ Faltu/app/src/main/java/com/zoomcar/prateek/faltu/activities/MainActivity.java
@@ -1,16 +1,22 @@
-package com.zoomcar.prateek.faltu;
+package com.zoomcar.prateek.faltu.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.zoomcar.prateek.faltu.R;
+
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
