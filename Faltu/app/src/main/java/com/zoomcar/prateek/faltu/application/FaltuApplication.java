0ae4a22e8b482d329836df4adb8ace5f276d61f1
Prateek Saini <prateek.saini@zoomcar.com>
2015-09-22 12:59:52 +0530

    Added a sample request queue

---
+++ Faltu/app/src/main/java/com/zoomcar/prateek/faltu/application/FaltuApplication.java
@@ -0,0 +1,10 @@
+package com.zoomcar.prateek.faltu.application;
+
+import android.app.Application;
+
+/**
+ * Created by Prateek on 22/09/15.
+ */
+public class FaltuApplication extends Application {
+
+}
---
+++ Faltu/app/src/main/java/com/zoomcar/prateek/faltu/network/RequestManager.java
@@ -0,0 +1,24 @@
+package com.zoomcar.prateek.faltu.network;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.HttpClientStack;
+import com.android.volley.toolbox.HttpStack;
+import com.android.volley.toolbox.Volley;
+import com.squareup.okhttp.OkHttpClient;
+
+/**
+ * Created by Prateek on 22/09/15.
+ */
+public class RequestManager {
+
+    private static RequestQueue mRequestQueue;
+
+    public static synchronized RequestQueue getRequestQueue(Context context){
+        if(mRequestQueue == null){
+            mRequestQueue = Volley.newRequestQueue(context);
+        }
+        return mRequestQueue;
+    }
+}
